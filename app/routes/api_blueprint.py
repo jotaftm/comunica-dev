6d95fe79f7b77b4b361b49d111a0031b88ee7a10
Diogo Nava <diogonsdf@gmail.com>
2021-12-08 11:28:17 -0300

    feat: added address routes

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -1,3 +1,6 @@
 from flask import Blueprint
+from app.routes.address_blueprint import bp_address
 
 bp_api = Blueprint("bp_api", __name__, url_prefix="/api")
+
+bp_api.register_blueprint(bp_address)

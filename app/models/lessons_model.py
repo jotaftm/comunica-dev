a9fea46be7c12d70b0e24a67c595cf0acc8b9a2f
luizrafa-federico <luizrafa.federico@gmail.com>
2021-12-09 11:46:04 -0300

    feat: lessons model, controller and route

---
+++ app/models/lessons_model.py
@@ -0,0 +1,32 @@
+from app.configs.database import db
+from dataclasses import dataclass
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import (
+    Integer,
+    String,
+    Boolean,
+    Text,
+)
+
+
+@dataclass
+class LessonModel(db.Model):
+
+    id: int
+    title: str
+    description: str
+    url_video: str
+    is_premium: bool
+    category_id: int
+
+    __tablename__ = "lessons"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    description = Column(Text, nullable=False)
+    url_video = Column(String, nullable=False)
+    is_premium = Column(Boolean, nullable=False)
+    category_id = Column(Integer, db.ForeignKey('categories.id'), nullable=False)
+
+    category = relationship('CategoryModel', backref="lessons", uselist=False)

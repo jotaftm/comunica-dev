ce436c24319b623f57affc77cb8f009880bc5d38
maykel nekel <maykelnekel@pop-os.localdomain>
2021-12-09 23:21:28 -0300

    fix: removing archive insomnia_blueprint

--- app/routes/insomnia_blueprint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from app.controllers.insomnia_controller import (
-    insomnia_controller
-) 
-
-bp = Blueprint('insomnia_bp', __name__, url_prefix='insomnia')
-
-bp.get('')(insomnia_controller)

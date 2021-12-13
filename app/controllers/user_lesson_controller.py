f1e82d9402b635ab0f3ee4d25a8097e0fb9cc540
Ariel Bernardes <ariel_bernardes@hotmail.com>
2021-12-13 20:22:32 -0300

    feat: user lesson controller

--- app/controllers/user_lesson_controller.py
+++ app/controllers/user_lesson_controller.py
@@ -1,7 +1,20 @@
 from flask import request, jsonify, current_app
 from app.models.user_lesson_model import UserLessonModel
 from http import HTTPStatus
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
-def get_one()
 
-def update_finished()
+@jwt_required()
+def update_finished(lesson_id):
+    user = get_jwt_identity()
+    result = {"finished": True}
+    user_lesson = UserLessonModel.query.filter(lesson_id=lesson_id, user_id=user['id']).update(result) 
+    
+    if not user_lesson:
+        return {"error": "User and lesson doesn't match!"}, HTTPStatus.NOT_FOUND
+
+    current_app.db.session.commit()
+    user_lesson_updated = UserLessonModel.query.get(lesson_id, user['id'])
+
+    return jsonify(user_lesson_updated)
+

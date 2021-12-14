1718f1feec3ff96b166ac6ebf00bf21de193dcbc
Ariel Bernardes <ariel_bernardes@hotmail.com>
2021-12-14 20:09:03 -0300

    feat: user lesson blueprint

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -6,6 +6,7 @@ from app.routes.lessons_blueprint import bp as lessons_bp
 from app.routes.users_blueprint import bp_users
 from app.routes.captchas_blueprint import bp_captchas
 from app.controllers.insomnia_controller import insomnia_controller
+from app.routes.user_lesson_blueprint import bp_user_lesson
 
 bp_api = Blueprint("bp_api", __name__, url_prefix="/api")
 
@@ -16,3 +17,4 @@ bp_api.register_blueprint(categories_bp)
 bp_api.register_blueprint(lessons_bp)
 bp_api.register_blueprint(bp_users)
 bp_api.register_blueprint(bp_captchas)
+bp_api.register_blueprint(bp_user_lesson)
---
+++ app/routes/user_lesson_blueprint.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+from app.controllers.user_lesson_controller import update_finished
+
+bp_user_lesson = Blueprint("bp_user_lesson", __name__, url_prefix="/users-lessons")
+
+bp_user_lesson.patch('')(update_finished)

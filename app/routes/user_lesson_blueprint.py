bc80b4f5673cbd1c0bfbd4b4ff97dd604555469d
Ariel Bernardes <ariel_bernardes@hotmail.com>
2021-12-14 20:19:04 -0300

    fix: int id blueprint

--- app/routes/user_lesson_blueprint.py
+++ app/routes/user_lesson_blueprint.py
@@ -4,4 +4,4 @@ from app.controllers.user_lesson_controller import update_finished
 
 bp_user_lesson = Blueprint("bp_user_lesson", __name__, url_prefix="/users-lessons")
 
-bp_user_lesson.patch('')(update_finished)
+bp_user_lesson.patch('/<int:id>')(update_finished)

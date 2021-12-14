9979741075ba8a4a805361bf1e45ada19d2ca7b1
José Flávio <jotaftm@gmail.com>
2021-12-14 11:08:19 -0300

    fix: Fixed user_lesson_controller filters.

--- app/routes/lessons_blueprint.py
+++ app/routes/lessons_blueprint.py
@@ -6,6 +6,7 @@ from app.controllers.lessons_controller import (
     get_lesson_by_id,
     delete_lesson
 ) 
+from app.controllers.user_lesson_controller import update_finished
 
 bp = Blueprint('lessons_bp', __name__, url_prefix='lessons')
 
@@ -14,3 +15,4 @@ bp.get('')(list_lessons)
 bp.get('/<int:id>')(get_lesson_by_id)
 bp.delete('/<int:id>')(delete_lesson)
 bp.patch('/<int:id>')(update_lesson)
+bp.patch('/finished/<int:id>')(update_finished)

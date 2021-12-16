aa52643d3e166c104d80aea3ce31cdb9c99aa790
José Flávio <jotaftm@gmail.com>
2021-12-16 11:45:04 -0300

    fix: delete archives obsolete.

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -5,11 +5,9 @@ from app.routes.categories_blueprint import bp as categories_bp
 from app.routes.lessons_blueprint import bp as lessons_bp
 from app.routes.users_blueprint import bp_users
 from app.routes.captchas_blueprint import bp_captchas
-from app.controllers.insomnia_controller import insomnia_controller
 
 bp_api = Blueprint("bp_api", __name__, url_prefix="/api")
 
-bp_api.get("")(insomnia_controller)
 bp_api.register_blueprint(bp_address)
 bp_api.register_blueprint(leads_bp)
 bp_api.register_blueprint(categories_bp)
